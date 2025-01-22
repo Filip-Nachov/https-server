311638937f2a67a543f59a190449aedda0081f83
Filip Nachov <fnachov@gmail.com>
2025-01-22 06:55:22 +0100

    made a socket boilerplate

---
+++ src/server.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+
+int main() {
+    int sockfd = socket(AF_LOCAL, SOCK_STREAM, 0);
+
+    // bind the socket
+    
+    // listen
+
+    // accept
+
+    return 0;
+}

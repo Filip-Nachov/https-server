d70a70fff364ed062f22476baae66d32f585bf02
Filip Nachov <fnachov@gmail.com>
2025-03-24 18:46:54 +0100

    moved all the declaring at the top of main

--- src/server.c
+++ src/server.c
@@ -9,23 +9,24 @@
 #define PORT 8080
 
 int main() {
-    int sockfd = socket(AF_LOCAL, SOCK_STREAM, 0);
+    // declare variables
+    int sockfd, new_sockfd, Sbind, Slisten;
+    struct sockaddr_in addr;
+
+    sockfd = socket(AF_LOCAL, SOCK_STREAM, 0);
     printf("SOCKET CREATED\n");
     printf("sockfd: %d\n", sockfd);
 
-    struct sockaddr_in addr;
     addr.sin_family = AF_INET;
 
     setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int));
 
-    int Sbind = bind(sockfd, (struct sockaddr*)&addr, sizeof(addr)); // bind
+    Sbind = bind(sockfd, (struct sockaddr*)&addr, sizeof(addr)); // bind
     printf("SOCKET BINDED\n");
 
-    // listen
-    int Slisten = listen(sockfd, 3);
+    Slisten = listen(sockfd, 3);
 
-    // accept
-    int new_sockfd = accept(sockfd, NULL, NULL);
+    new_sockfd = accept(sockfd, NULL, NULL);
 
     return 0;
 }
